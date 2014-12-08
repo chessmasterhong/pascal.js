cd98021a69d45613984b7173b095ae922de09a82
Kevin Chan <chessmasterhong@yahoo.com>
2014-12-08 01:23:45 -0500

    Major refactoring for scanner.js and main.js.

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,56 @@ require.config({
 define([
     'require',
     'modules/scanner'
-], function(require) {
+], function(require, Scanner) {
     'use strict';
-
-    var scanner = require('modules/scanner');
 
     // Check if File API is supported in browser
     if(File && FileReader) {
+        var readFile = function(evt) {
+            // Retrieve first file from FileList object (if multiple files selected)
+            var file = evt.target.files[0];
+
+            if(file) {
+                // Instantiate FileReader object to read file data to memory
+                var reader = new FileReader();
+
+                // Fire onprogress event while reading file
+                reader.onprogress = function(e) {
+                    if(e.lengthComputable) {
+                        var loaded = Math.floor((e.loaded / e.total) * 100);
+                        if(loaded < 1) {
+                            console.log('Loading file: ' + loaded + '%');
+                        }
+                    }
+                };
+
+                // Fire onload event when file has been successfully read
+                reader.onload = function(e) {
+                    console.log('File was successfully loaded.');
+                    console.log('File Name: ' + file.name);
+                    console.log('File Type: ' + file.type);
+                    console.log('File Size: ' + file.size + ' bytes');
+
+                    var scanner = new Scanner(e.target.result);
+                    var character = scanner.get();
+                };
+
+                // Fire onerror event if error occurred while reading file
+                reader.onerror = function(e) {
+                    if(e.target.error.name === 'NotReadableError') {
+                        alert('ERROR: There was an error reading the file.');
+                    }
+                };
+
+                // Read file as a text string decoded as UTF-8
+                reader.readAsText(file, 'utf-8');
+            } else {
+                alert('ERROR: The file cannot be loaded.');
+            }
+        };
+
         // Detect changes (loaded file) in fileInput and run readFile() on change
-        document.getElementById('fileInput').addEventListener('change', scanner.readFile, false);
+        document.getElementById('fileInput').addEventListener('change', readFile, false);
     } else {
         alert('ERROR: The File APIs are not fully supported in your browser.');
     }
--- scripts/modules/scanner.js
+++ scripts/modules/scanner.js
@@ -1,47 +1,20 @@
-define(function() {
+define([
+    'modules/character'
+], function(Character) {
     'use strict';
 
-    function Scanner() {}
+    function Scanner(srcText) {
+        this.srcText   = srcText;
+        this.finalIndex = this.srcText.length - 1;
+        this.srcIndex  = -1;
+        this.lineIndex = 0;
+        this.colIndex  = -1;
+    }
 
-    Scanner.readFile = function(evt) {
-        // Retrieve first file from FileList object (if multiple files selected)
-        var file = evt.target.files[0];
+    Scanner.prototype = {};
 
-        if(file) {
-            // Instantiate FileReader object to read file data to memory
-            var reader = new FileReader();
+    Scanner.prototype.get = function() {
 
-            // Fire onprogress event while reading file
-            reader.onprogress = function(e) {
-                if(e.lengthComputable) {
-                    var loaded = Math.floor((e.loaded / e.total) * 100);
-                    if(loaded < 1) {
-                        console.log('Loading file: ' + loaded + '%');
-                    }
-                }
-            };
-
-            // Fire onload event when file has been successfully read
-            reader.onload = function(e) {
-                console.log('File was successfully loaded.');
-                console.log('File Name: ' + file.name);
-                console.log('File Type: ' + file.type);
-                console.log('File Size: ' + file.size + ' bytes');
-                console.log('File Contents (first 100 chars): \n' + e.target.result.substr(0, 100));
-            };
-
-            // Fire onerror event if error occurred while reading file
-            reader.onerror = function(e) {
-                if(e.target.error.name === 'NotReadableError') {
-                    alert('ERROR: There was an error reading the file.');
-                }
-            };
-
-            // Read file as a text string decoded as UTF-8
-            reader.readAsText(file, 'utf-8');
-        } else {
-            alert('ERROR: The file cannot be loaded.');
-        }
     };
 
     return Scanner;
