b32a3bf84d14e5fe11fcba744fb4420ebc014b4e
Kevin Chan <chessmasterhong@yahoo.com>
2014-12-08 17:32:13 -0500

    Added JSHint to Node dev-dependencies. Added lint gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
     shell = require('gulp-shell');
 
+gulp.task('lint', function() {
+    return gulp.src('./scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(require('jshint-stylish')));
+});
 
 gulp.task('docs', function() {
     return gulp.src('./README.md')
