6f0d97df6983f2eac9654703429e00517c9b3709
Kevin Chan <chessmasterhong@yahoo.com>
2014-12-07 19:38:22 -0500

    Added onprogress and onerror handling to FileReader().

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,24 @@
     'use strict';
 
     function readFile(evt) {
-        // Retrieve first file from FileList object
-        var file = evt.target.files[0]; // Obtain first file if multiple files selected
+        // Retrieve first file from FileList object (if multiple files selected)
+        var file = evt.target.files[0];
 
         if(file) {
             // Instantiate FileReader object to read file data to memory
             var reader = new window.FileReader();
 
-            // Fire onload event when when data can be accessed
+            // Fire onprogress event while file is being read
+            reader.onprogress = function(e) {
+                if(e.lengthComputable) {
+                    var loaded = Math.floor((e.loaded / e.total) * 100);
+                    if(loaded < 1) {
+                        console.log('Loading file: ' + loaded + '%');
+                    }
+                }
+            };
+
+            // Fire onload event when file has been successfully read
             reader.onload = function(e) {
                 console.log('File was successfully loaded.');
                 console.log('File Name: ' + file.name);
@@ -18,10 +28,16 @@
                 console.log('File Contents (first 100 chars): \n' + e.target.result.substr(0, 100));
             };
 
+            reader.onerror = function(e) {
+                if(e.target.error.name === 'NotReadableError') {
+                    console.log('There was an error reading the file.');
+                }
+            };
+
             // Read file as a text string decoded as UTF-8
             reader.readAsText(file);
         } else {
-            console.log('Failed to load file.');
+            console.log('The file cannot be loaded.');
         }
     }
 
